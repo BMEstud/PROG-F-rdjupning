83b5259510b3f2e85cf170c5d00384d92bda0e13
Mattias Seth <mattias.m.seth@gmail.com>
2018-11-08 16:11:06 +0100

    Skolprojekt

--- app/src/main/java/com/example/suduko/MainActivity.kt
+++ app/src/main/java/com/example/suduko/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.suduko
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        //ndnefenfef f r
+        //ndnefenfef f rntojn54g 544g45gb45hgb45ijgn54kjg 4kjgnb4jgb54 gh4
 
 
 
